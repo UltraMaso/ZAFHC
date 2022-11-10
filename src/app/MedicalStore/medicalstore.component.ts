13c961abcde5559390c1bc70ee5cfa334d807045
Sechaba Maso <sechaba@rqtech.co.za>
2022-11-10 08:48:46 +0200

    initial commit

---
+++ src/app/MedicalStore/medicalstore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medicalstore',
+  templateUrl: './medicalstore.component.html',
+  styleUrls: ['./medicalstore.component.scss']
+})
+export class MedicalstoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  login: {username?: string, password?: string} = {};
+  constructor(   private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+  onSubmit(){
+    console.log("submit closes");
+    this.router.navigate(['/dashboard']);
+  }
+
+}
